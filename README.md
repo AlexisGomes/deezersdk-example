37c81f89f74010b2b9d6d871e22be75c30ef00fe
Alexis Gomes <alexis.gomes19@gmail.com>
2020-04-15 09:07:58 +0200

    READ ME

--- README.md
+++ README.md
@@ -1,28 +1,28 @@
-# Creating the project 
-## Set up project
+**This is a small flask project to use the [deezersdk](https://github.com/AlexisGomes/deezersdk)** 
+
+## Set up the project
 - Create your virtual env
 - Install packages
+    ```bash
     pip install Flask
     pip install deezersdk
-    
-- create app.py
-```bash
-from flask import Flask
-from deezersdk import deezersdk
+    ```    
+- Create a file settings.py following the example in `settings.example.py` 
+and replace it with your Deezer's application informations. 
+- Start the server with 
+    ```bash 
+    python app.py
+    ```
 
-app = Flask(__name__)
+## Login to Deezer's API
+My local server run on `http://127.0.0.1:5000/`
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+- Go to the default route `/`
+- This will open a pop to login to your Deezer account.
+- You will then be redirected to `/deezer/login` if it's a success
+- This will redirect you to `/deezer`, witch will find your first playlist and play it.
 
-if __name__ == '__main__':
-    app.run()
-```
 
-- now add a settings file with your DEEZER_APP_ID and your DEEZER_APP_SECRET
+**For more informations and details on the use of the sdk, you take a look at the [READ ME](https://github.com/AlexisGomes/deezersdk)** 
 
-## Login to deezer
-
-Get code
  
--- app.py
+++ app.py
@@ -1,5 +1,5 @@
+from deezersdk.deezersdk import Deezer
 from flask import Flask, redirect, request
-from deezersdk import deezersdk
 from settings import DEEZER_APP_ID, DEEZER_APP_SECRET, DEEZER_REDIRECT_URI
 
 app = Flask(__name__)
@@ -7,14 +7,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def default():
-    url = deezersdk.Deezer.get_oauth_login_url(app_id=DEEZER_APP_ID, redirect_uri=DEEZER_REDIRECT_URI)
+    url = Deezer.get_oauth_login_url(app_id=DEEZER_APP_ID, redirect_uri=DEEZER_REDIRECT_URI)
     return redirect(url)
 
 
 @app.route('/deezer/login')
 def deezer_login_completed():
     code = request.args.get('code')
-    result = deezersdk.Deezer.get_oauth_token(app_id=DEEZER_APP_ID, app_secret=DEEZER_APP_SECRET, code=code)
+    result = Deezer.get_oauth_token(app_id=DEEZER_APP_ID, app_secret=DEEZER_APP_SECRET, code=code)
     if result == 'wrong code':
         return redirect('/')
     return redirect(f'/deezer?token={result}')
@@ -23,10 +23,10 @@ def deezer_login_completed():
 @app.route('/deezer')
 def deezer():
     token = request.args.get('token')
-    deezer = deezersdk.Deezer(app_id=DEEZER_APP_ID, app_secret=DEEZER_APP_SECRET, token=token)
+    dz = Deezer(app_id=DEEZER_APP_ID, app_secret=DEEZER_APP_SECRET, token=token)
 
-    playlists = deezer.get_my_playlists()
-    url = deezer.get_widget(playlist=playlists[0])
+    playlists = dz.get_my_playlists()
+    url = dz.get_widget(playlist=playlists[0])
     return redirect(url)
 
 
